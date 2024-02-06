cabf329f17123ddbe478b6c28b38696fb87e4b89
Michael-Njoroge <mikethecoder12@gmail.com>
2024-02-06 03:15:55 +0300

    working on home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ import { NavLink, Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <section className="home-wrapper-1 py-3">
+      <section className="home-wrapper-1 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner position-relative p-3">
+              <div className="main-banner position-relative">
                 <img
                   src="images/main-banner-1.jpg"
                   alt="main-banner"
@@ -24,7 +24,7 @@ const Home = () => {
             </div>
             <div className="col-6">
               <div className="d-flex gap-10 flex-wrap justify-content-between align-items-center">
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-01.jpg"
                     alt="small-banner"
@@ -38,7 +38,7 @@ const Home = () => {
                     </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-02.jpg"
                     alt="small-banner"
@@ -52,7 +52,7 @@ const Home = () => {
                     </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-03.jpg"
                     alt="small-banner"
@@ -66,7 +66,7 @@ const Home = () => {
                     </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-04.jpg"
                     alt="small-banner"
@@ -79,6 +79,117 @@ const Home = () => {
                       From $999.00 <br /> or $47.62/mo
                     </p>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all orders over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Daily Suprise Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Support 24/7 </h6>
+                    <p className="mb-0">Shop with a expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory direct price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Secure Payement</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories flex-wrap d-flex align-items-center align-items-center justify-content-between">
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>22 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Head Phones</h6>
+                    <p>8 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Cameras</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Smart Tv</h6>
+                    <p>22 Items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Head Phones</h6>
+                    <p>8 Items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <h6>Music & Gaming</h6>
+                    <p>10 Items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
                 </div>
               </div>
             </div>
