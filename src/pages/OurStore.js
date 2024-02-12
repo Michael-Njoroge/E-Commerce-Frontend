3e06244181740e771af533fe911a8321c58dd554
Michael-Njoroge <mikethecoder12@gmail.com>
2024-02-12 23:57:49 +0300

    complete random products card

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
 
 const OurStore = () => {
   return (
@@ -143,6 +144,52 @@ const OurStore = () => {
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Products</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="random"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="random"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="col-9"></div>
