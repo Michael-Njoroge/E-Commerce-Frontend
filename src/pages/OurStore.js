ecb7b3336c1d12d8d1999927d9cadcb11e8c4598
Michael-Njoroge <mikethecoder12@gmail.com>
2024-02-12 19:59:10 +0300

    complete shop by categories card

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -7,6 +7,36 @@ const OurStore = () => {
     <>
       <Meta title="Our Store" />
       <BreadCrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop By Categories</h3>
+
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter By</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tag</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Products</h3>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
