bc20df60e497fe0bc6f9770dc9f1409dc5dbca94
Michael-Njoroge <mikethecoder12@gmail.com>
2024-02-15 11:51:05 +0300

    complete contact us page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,111 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneCall } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
+import { BiInfoCircle } from "react-icons/bi";
 
 const Contact = () => {
   return (
     <>
-      <Meta title="E-Commerce | Contact" />
-      <BreadCrumb title="Contact" />
+      <Meta title="E-Commerce | Contact Us" />
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8175095926376!2d36.82096247381016!3d-1.2833529987044474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f110049a8886b%3A0xc646c93b7d72eafd!2sNairobi%20town!5e0!3m2!1sen!2ske!4v1707972148217!5m2!1sen!2ske"
+                width="600"
+                height="450"
+                title="Google Map of Nairobi Town"
+                className="border-0 w-100"
+                allowFullscreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name=""
+                        id=""
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        name=""
+                        id=""
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        name=""
+                        id=""
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="3"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">
+                          Hno. 277, Near village park, Mombase road, Nairobi,
+                          Kenya
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+254716002152">+254 716002152</a>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:mikethecoder12@gmail.com">
+                          mikethecoder12@gmaill.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10am - 8pm</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
