282096485508179097f28391b69e07427cb27626
Michael-Njoroge <mikethecoder12@gmail.com>
2024-03-03 23:03:47 +0300

    working on whishlist

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -7,6 +7,34 @@ const Wishlist = () => {
     <>
       <Meta title="E-Commerce | Wishlist" />
       <BreadCrumb title="Wishlist" />
+      <div className="wishlist-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="wishlist-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross img-fluid"
+                />
+                <div className="wishlist-card-image">
+                  <img
+                    src="images/watch.jpg"
+                    alt="watch"
+                    className="img-fluid w-100"
+                  />
+                </div>
+                <div className="py-3 px-3">
+                  <h5 className="title">
+                    Honr T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+3GB Tablet
+                  </h5>
+                  <h6 className="price">$100</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
