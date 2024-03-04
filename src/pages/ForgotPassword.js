bafdee47390c62b0a5b8c4f6faaf67439150f68a
Michael-Njoroge <mikethecoder12@gmail.com>
2024-03-04 22:37:42 +0300

    create signup,login and forgot password pages

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const ForgotPassword = () => {
+  return (
+    <>
+      <Meta title="E-Commerce | ForgotPassword" />
+      <BreadCrumb title="ForgotPassword" />
+    </>
+  );
+};
+
+export default ForgotPassword;
---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const Signup = () => {
+  return (
+    <>
+      <Meta title="E-Commerce | Signup" />
+      <BreadCrumb title="Signup" />
+    </>
+  );
+};
+
+export default Signup;
