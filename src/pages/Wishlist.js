a342b63de4d003291f9d4855f21bef30f731ba34
Michael-Njoroge <mikethecoder12@gmail.com>
2024-03-04 22:29:34 +0300

    complete wishlist page

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -32,6 +32,50 @@ const Wishlist = () => {
                 </div>
               </div>
             </div>
+            <div className="col-3">
+              <div className="wishlist-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross img-fluid"
+                />
+                <div className="wishlist-card-image">
+                  <img
+                    src="images/watch.jpg"
+                    alt="watch"
+                    className="img-fluid w-100"
+                  />
+                </div>
+                <div className="py-3 px-3">
+                  <h5 className="title">
+                    Honr T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+3GB Tablet
+                  </h5>
+                  <h6 className="price">$100</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="wishlist-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross img-fluid"
+                />
+                <div className="wishlist-card-image">
+                  <img
+                    src="images/watch.jpg"
+                    alt="watch"
+                    className="img-fluid w-100"
+                  />
+                </div>
+                <div className="py-3 px-3">
+                  <h5 className="title">
+                    Honr T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+3GB Tablet
+                  </h5>
+                  <h6 className="price">$100</h6>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
