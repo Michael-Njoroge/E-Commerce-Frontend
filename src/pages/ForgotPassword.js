90b093bcae4f667d7cf22a63b26c7a82548894bf
Michael-Njoroge <mikethecoder12@gmail.com>
2024-03-05 00:04:53 +0300

    minor corrections

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -10,33 +10,35 @@ const ForgotPassword = () => {
       <BreadCrumb title="ForgotPassword" />
 
       <div className="login-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Reset Your Password</h3>
-              <p className="mt-2 mb-3 text-center">
-                We will send you an email to reset your password
-              </p>
-              <form action="" className="d-flex flex-column gap-15">
-                <div>
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    placeholder="Email"
-                    id=""
-                  />
-                </div>
-
-                <div>
-                  <div className="mt-3 d-flex justify-content-center flex-column gap-15 align-items-center">
-                    <button className="button border-0" type="submit">
-                      Submit
-                    </button>
-                    <Link to="/login">Cancel</Link>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">Reset Your Password</h3>
+                <p className="mt-2 mb-3 text-center">
+                  We will send you an email to reset your password
+                </p>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      placeholder="Email"
+                      id=""
+                    />
                   </div>
-                </div>
-              </form>
+
+                  <div>
+                    <div className="mt-3 d-flex justify-content-center flex-column gap-15 align-items-center">
+                      <button className="button border-0" type="submit">
+                        Submit
+                      </button>
+                      <Link to="/login">Cancel</Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,41 +10,43 @@ const Login = () => {
       <BreadCrumb title="Account" />
 
       <div className="login-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Login</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <div>
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    placeholder="Email"
-                    id=""
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    placeholder="Password"
-                    id=""
-                  />
-                </div>
-                <div>
-                  <Link to="/forgot-password">Forgot Password?</Link>
-                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <button className="button border-0" type="submit">
-                      Login
-                    </button>
-                    <Link className="signup button" to="/signup">
-                      Signup
-                    </Link>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">Login</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      placeholder="Email"
+                      id=""
+                    />
                   </div>
-                </div>
-              </form>
+                  <div className="mt-1">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Password"
+                      id=""
+                    />
+                  </div>
+                  <div>
+                    <Link to="/forgot-password">Forgot Password?</Link>
+                    <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                      <button className="button border-0" type="submit">
+                        Login
+                      </button>
+                      <Link className="signup button" to="/signup">
+                        Signup
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
