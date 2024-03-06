f74063a74e1c29327bab9f06f9420d795794fa14
Michael-Njoroge <mikethecoder12@gmail.com>
2024-03-06 22:42:13 +0300

    create single blog page

---
+++ src/pages/SingleBlog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const SingleBlog = () => {
+  return (
+    <>
+      <Meta title="E-Commerce | SingleBlog" />
+      <BreadCrumb title="SingleBlog" />
+    </>
+  );
+};
+
+export default SingleBlog;
