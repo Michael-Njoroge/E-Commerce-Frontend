02068717e1aa41f7529fcb3f303d3755932d69ac
Michael Nyambura <mikethecoder12@gmail.com>
2024-06-17 11:22:30 +0300

    complete cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,8 +2,9 @@ import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import BlogCard from "../components/BlogCard";
-import watch from "../images/watch.jpg"
-import {AiFillDelete} from "react-icons/ai"
+import watch from "../images/watch.jpg";
+import {AiFillDelete} from "react-icons/ai";
+import {Link} from "react-router-dom";
 
 const Cart = () => {
   return (
@@ -26,9 +27,9 @@ const Cart = () => {
               			<img src={watch} className="img-fluid" alt="product image"/>
               		</div>
               		<div className="w-75">
-              			<h5 className="title">sfdwfds</h5>
-              			<p className="color">sfdwfds</p>
-              			<p className="size">sfdwfds</p>
+              			<p>Kids headphones bulk 10 pack multi colored for students</p>
+              			<p>Size : sfdwfds</p>
+                    <p>Color : sfdwfds</p>
               		</div>
               	</div>
               	<div className="cart-col-2">
@@ -39,38 +40,22 @@ const Cart = () => {
               			<input type="number" className="form-control" name="" id="" min={1} max={10} />
               		</div>
               		<div>
-              			<AiFillDelete/>
+              			<AiFillDelete className="text-danger"/>
               		</div>
               	</div>
               	<div className="cart-col-4">
               		<h5 className="price">$ 100</h5>
               	</div>
               </div>
-              <div className="cart-data mb-2 py-3 d-flex align-items-center justify-content-between">
-              	<div className="cart-col-1 gap-15 d-flex align-items-center">
-              		<div className="w-25">
-              			<img src={watch} className="img-fluid" alt="product image"/>
-              		</div>
-              		<div className="w-75">
-              			<h5 className="title">sfdwfds</h5>
-              			<p className="color">sfdwfds</p>
-              			<p className="size">sfdwfds</p>
-              		</div>
-              	</div>
-              	<div className="cart-col-2">
-              		<h5 className="price">$ 100</h5>
-              	</div>
-              	<div className="cart-col-3 d-flex align-items-center gap-15">
-              		<div>
-              			<input type="number" className="form-control" name="" id="" min={1} max={10} />
-              		</div>
-              		<div>
-              			<AiFillDelete/>
-              		</div>
-              	</div>
-              	<div className="cart-col-4">
-              		<h5 className="price">$ 100</h5>
-              	</div>
+            </div>
+            <div className="col-12 py-2 mt-4">
+              <div className="d-flex justify-content-between align-items-baseline">
+                <Link className="button" to="/product">Continue To Shopping</Link>
+                <div className="d-flex align-items-end flex-column">
+                  <h4>SubTotal : $1000</h4>
+                  <p>Taxes and shipping calculated at checkout</p>
+                  <Link className="button" to="/checkout">Checkout</Link>
+                </div>
               </div>
             </div>
           </div>
