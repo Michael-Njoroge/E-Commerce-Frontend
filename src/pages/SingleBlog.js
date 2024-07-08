9421ba8c3053b18e5716d8737f3610a3712f9e3d
Michael Nyambura <mikethecoder12@gmail.com>
2024-07-08 07:01:54 +0300

    complete blog listing and single blog view functionality

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -29,14 +29,14 @@ const SingleBlog = () => {
       <Container class1="blog-wrapper home-wrapper-2 py-5">
           <div className="row">
             <div className="col-12">
-              <div className="single-blog-card">
+              <div className="single-blog-card text-center">
                 <Link to="/blogs" className="d-flex align-items-center gap-10">
                   <HiOutlineArrowLeft className="fs-4" />
                   Go Back
                 </Link>
                 <h3 className="title">{singleBlog?.title}</h3>
                 <img
-                  className="w-100 my-4 img-fluid"
+                  className="w-50 my-4 img-fluid"
                   src={singleBlog?.images[0]?.file_url}
                   alt="blogs"
                 />
