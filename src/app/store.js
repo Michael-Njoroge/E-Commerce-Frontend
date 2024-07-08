ca67ac33ed5814946d0d9dec2f2549987398ffab
Michael Nyambura <mikethecoder12@gmail.com>
2024-07-08 14:18:42 +0300

    complete getting user side sending of enquiries

--- src/app/store.js
+++ src/app/store.js
@@ -9,6 +9,6 @@ export const store = configureStore({
     auth: authReducer,
     product: productReducer,
     blog: blogReducer,
-    contact: enquiryRedcer
+    contact: enquiryReducer
   },
 });
