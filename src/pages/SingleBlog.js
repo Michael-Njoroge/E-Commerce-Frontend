92375e67b38fb7fc548ce717d6bf75d5cb593d07
Michael Nyambura <mikethecoder12@gmail.com>
2024-07-08 15:42:49 +0300

    complete getting user side sending of enquiries

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -28,20 +28,35 @@ const SingleBlog = () => {
       <BreadCrumb title={singleBlog?.title} />
       <Container class1="blog-wrapper home-wrapper-2 py-5">
           <div className="row">
-            <div className="col-12">
-              <div className="single-blog-card text-center">
+          <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Find By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-9">
+            <div className="row">
+              <div className="single-blog-card">
                 <Link to="/blogs" className="d-flex align-items-center gap-10">
                   <HiOutlineArrowLeft className="fs-4" />
                   Go Back
                 </Link>
                 <h3 className="title">{singleBlog?.title}</h3>
                 <img
-                  className="w-50 my-4 img-fluid"
+                  className="w-100 my-4 img-fluid"
                   src={singleBlog?.images[0]?.file_url}
                   alt="blogs"
                 />
                 <p dangerouslySetInnerHTML={{ __html: singleBlog?.description }}>
                 </p>
+              </div>
               </div>
             </div>
           </div>
