dccbd9a1bb33bfad4b7210acd042fa38a2d16e1a
Michael Nyambura <mikethecoder12@gmail.com>
2024-07-10 09:38:00 +0300

    complete adding to cart and getting usercart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import watch from "../images/watch.jpg";
 import {AiFillDelete} from "react-icons/ai";
 import {Link} from "react-router-dom";
 import Container from "../components/Container";
@@ -10,14 +9,12 @@ import {useDispatch, useSelector} from 'react-redux'
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const cartState = useSelector((state) => state.product.userCart.products);
-
-console.log("cartState",cartState)
-  const getUserCart = () => {
-    dispatch(getCart());
-  };
+  const cartState = useSelector((state) => state.product.userCart);
 
   useEffect(() => {
+    const getUserCart = () => {
+      dispatch(getCart());
+    };
     getUserCart()
   }, []);
   return (
@@ -33,35 +30,48 @@ console.log("cartState",cartState)
               	<h4 className="cart-col-3">Quantity</h4>
               	<h4 className="cart-col-4">Total</h4>
               </div>
+        {/*      {
+                (!cartState || cartState.length === 0) && (
+                  <div className="text-center fs-3">
+                    Your cart is empty
+                  </div>
+                )
+              }*/}
               {
                 cartState && cartState?.products.map((item, index) => {
-
-                  <div key={index} className="cart-data mb-2 py-3 d-flex align-items-center justify-content-between">
-                    <div className="cart-col-1 gap-15 d-flex align-items-center">
-                      <div className="w-25">
-                        <img src={watch} className="img-fluid" alt="product"/>
-                      </div>
-                      <div className="w-75">
-                        <p>{item?.product?.title}</p>
-                        <p>Size : sfdwfds</p>
-                        <p>Color : sfdwfds</p>
-                      </div>
+                  {/*{console.log(item)}*/}
+                  return( 
+                    <div key={index} className="cart-data mb-2 py-3 d-flex align-items-center justify-content-between">
+                        <div className="cart-col-1 gap-15 d-flex align-items-center">
+                          <div className="w-25">
+                            <img src={item?.product?.images[0]?.file_url} className="img-fluid" alt="product"/>
+                          </div>
+                          <div className="w-75">
+                            <p>{item?.product?.title}</p>
+                            {/*<p>Size : sfdwfds</p>*/}
+                            <p className="d-flex gap-3">Color : 
+                                <ul className="colors ps-0">
+                                  <li style={{backgroundColor:item?.color?.title}}></li>
+                                </ul>
+                              </p>
+                          </div>
+                        </div>
+                        <div className="cart-col-2">
+                          <h5 className="price">$ {item?.product?.price}</h5>
+                        </div>
+                        <div className="cart-col-3 d-flex align-items-center gap-15">
+                          <div>
+                            <input type="number" value={item?.quantity} className="form-control" name="" id="" min={1} max={10} />
+                          </div>
+                          <div>
+                            <AiFillDelete className="text-danger"/>
+                          </div>
+                        </div>
+                        <div className="cart-col-4">
+                          <h5 className="price">$ {item?.quantity * item?.product?.price}</h5>
+                        </div>
                     </div>
-                    <div className="cart-col-2">
-                      <h5 className="price">$ {cartState?.price}</h5>
-                    </div>
-                    <div className="cart-col-3 d-flex align-items-center gap-15">
-                      <div>
-                        <input type="number" className="form-control" name="" id="" min={1} max={10} />
-                      </div>
-                      <div>
-                        <AiFillDelete className="text-danger"/>
-                      </div>
-                    </div>
-                    <div className="cart-col-4">
-                      <h5 className="price">$ 100</h5>
-                    </div>
-              </div>
+                )
                 })
               }
               
@@ -70,7 +80,7 @@ console.log("cartState",cartState)
               <div className="d-flex justify-content-between align-items-baseline">
                 <Link className="button" to="/product">Continue To Shopping</Link>
                 <div className="d-flex align-items-end flex-column">
-                  <h4>SubTotal : $ {cartState?.cartTotal}</h4>
+                  <h4>Sub Total : $ {cartState?.cartTotal}</h4>
                   <p>Taxes and shipping calculated at checkout</p>
                   <Link className="button" to="/checkout">Checkout</Link>
                 </div>
